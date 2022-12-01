013ebcb4529c314d84daad6ad570a2992b9e68cb
user <user@i7B-25>
2022-12-01 16:40:36 +0900

    처음 올리는 웹 프로젝트

---
+++ src/main/java/com/web/test.java
@@ -0,0 +1,18 @@
+package com.web;
+
+public class test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	
+		for(int i=1;i<=20;i++) {
+			if(i%10%3==0) {
+				System.out.println("짝");
+			} else {
+				System.out.println(i);
+			}
+		}
+	}
+}
+
+
